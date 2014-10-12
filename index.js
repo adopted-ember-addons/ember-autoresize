c13b3ab6212fa7b200ea8d4d4fd6b7177e2925ce
Tim Evans <tim-evans@sproutcore.com>
2014-10-11 23:26:46 -0400

    fix import of dom-ruler in index.js for ember-autoresize

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ module.exports = {
   name: 'ember-autoresize',
   included: function (app) {
     this._super.included(app);
-    app.import('bower_components/dom-ruler/dist/dom-ruler.amd.js');
+    app.import('bower_components/dom-ruler/dist/dom-ruler.amd.js', {
+      exports: {
+        'dom-ruler': ['default']
+      }
+    });
   }
 };
