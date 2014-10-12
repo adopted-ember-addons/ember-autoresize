fe34c979cea7934fb5c33c7c858cc665c63d3006
Tim Evans <tim-evans@sproutcore.com>
2014-10-11 23:16:06 -0400

    convert ember-autoresize into an ember-cli addon

---
+++ app/initializers/autoresize.js
@@ -0,0 +1,7 @@
+import "ember-autoresize/ext/text-field";
+import "ember-autoresize/ext/text-area";
+
+export default {
+  name: "autoresize",
+  initialize: function () {}
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'ember-autoresize',
+  included: function (app) {
+    this._super.included(app);
+    app.import('bower_components/dom-ruler/dist/dom-ruler.amd.js');
+  }
+};
