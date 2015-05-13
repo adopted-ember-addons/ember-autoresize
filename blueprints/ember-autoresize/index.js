2b42e725a543df1c85f77fc2b7c52d6416703575
Jordan Hawker <hawker.jordan@gmail.com>
2015-05-13 11:47:35 -0700

    Upgrade dom-ruler version in blueprint

--- blueprints/ember-autoresize/index.js
+++ blueprints/ember-autoresize/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('dom-ruler#0.1.4');
+    return this.addBowerPackageToProject('dom-ruler#0.1.5');
   }
 };
