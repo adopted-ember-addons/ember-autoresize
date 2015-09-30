4f53140c0c330a2479a6040bf83c4374ec831407
Tim Evans <tevans@movableink.com>
2015-09-30 12:46:02 -0400

    update blueprint dependencies for dom-ruler

--- blueprints/ember-autoresize/index.js
+++ blueprints/ember-autoresize/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('dom-ruler', '0.1.8');
+    return this.addBowerPackageToProject('dom-ruler', '0.1.12');
   }
 };
