28d09acbb6e86ee7a6c55c73618ea3b529b329be
Tim Evans <tim_evans@condenast.com>
2017-11-28 11:51:06 -0500

    remove dependency on ember-cli-node-assets and require ember-cli 2.15 or greater when installing this version

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 /* eslint-env node */
 'use strict';
+let VersionChecker = require('ember-cli-version-checker');
 
 module.exports = {
   name: 'ember-autoresize',
-  options: {
-    nodeAssets: {
-      'dom-ruler': {
-        srcDir: 'dist/amd',
-        import: ['dom-ruler.js']
-      }
-    }
-  },
 
   included: function (app) {
     this._super.included.apply(this, arguments);
-    app.import('vendor/dom-ruler/dom-ruler.js', {
+    let checker = new VersionChecker(this);
+
+    checker.for('ember-cli').assertAbove('2.15.0', '`ember-cli` 2.15 or greater is required to use this version of ember-autoresize. Use ember-autoresize@0.6.1 if you\'re using an older version of ember-cli.');
+
+    app.import('node_modules/dom-ruler/dist/amd/dom-ruler.js', {
       exports: {
         'dom-ruler': ['default']
       }
